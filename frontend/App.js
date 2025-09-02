912b7cc4a9dd36e3180a25ad123144e66a667047
aryan557 <aryanraj43004@gmail.com>
2025-09-02 16:21:58 +0530

    Added login/signup toggle with bootstrap UI

--- frontend/App.js
+++ frontend/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Home from "./src/pages/Home";
 import Auth from "./src/pages/Auth";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 
 
